542a3617d526c69a9f3615d7335481e90464cbf9
Mohammad Mussab <mussabna9@gmail.com>
2025-11-24 13:27:35 +0500

    removed image sharped

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone', // Enable standalone build for Docker
   typescript:{
     ignoreBuildErrors:true
-  },
-  // Disable Sharp image optimization to prevent Bus error on Azure
-  images: {
-    unoptimized: true
   }
 };
 
